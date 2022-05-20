ccbc8e51e8c8c5e784f0372d58a9a7c129e31967
erica201193 <erica.mancini20@gmail.com>
2022-05-20 16:49:00 +0200

    prove js

--- js/app.js
+++ js/app.js
@@ -19,15 +19,21 @@ Buon lavoro e buon divertimento! :faccia_leggermente_sorridente: */
 for (let i = 0; i <= 100; i++) {
     
     if (i % 3 === 0) {
-        console.log("Fizz")
+        console.log("Fizz");
     }
 
     if (i % 5 === 0) {
-        console.log("Buzz")
+        console.log("Buzz");
     }
 
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
     }
+
+    if (i) {
+        console.log(i);
+    }
+
+    
 
 }
